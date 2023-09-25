ebef4d6fab6dfed2779f3204de1c13fda5f933b6
Jack Weber <jackweber142@gmail.com>
2023-09-25 11:50:52 -0400

    flood fill finished

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ impl Point {
     }
 }
 
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct Color {
     r: u8,
@@ -44,6 +45,8 @@ struct Image {
 /// Gathers every group of connected pixels with the same colors
 /// Only gathers groups of colors we care about
 fn gather_glyphs(image: Image, we_care: Vec<Color>) -> Vec<Glyph> {
+    let mut glyph_list = vec![];
+
     // Creates a list to see if we have visited each pixel
     // (Foor flood fill algorithm)
     let mut visited = vec![false; image.pixels.len()];
@@ -61,37 +64,71 @@ fn gather_glyphs(image: Image, we_care: Vec<Color>) -> Vec<Glyph> {
                 continue;
             }
 
-            let connected_pixels: Vec<Pixel> = vec![];
+            if !visited[get_index(pixel.point, image.width)] {
+                let mut glyph = Glyph {color: pixel.color, pixels: vec![], u_left: Point::zero(), l_right: Point::zero()};           
+                glyph = flood_fill(&image, glyph, &mut visited, pixel.clone());
 
-            let glyph = Glyph { color: pixel.color, pixels: vec![], u_left: Point::zero(), l_right: Point::zero()};           
-            flood_fill(&image, glyph, &mut visited, pixel.color, pixel.point);
+                glyph_list.push(glyph);
+            }
 
         }
     
     }
-    return vec![];
+    return glyph_list;
 }
 
-fn flood_fill(image: &Image, mut glyph: Glyph,mut visited: &mut Vec<bool>, color: Color, pos: Point) -> Glyph {
-    let index = get_index(pos, image.width);
-    if visited[index] == true {
-        print!("pooop");
-        return glyph; 
+
+fn flood_fill(image: &Image, mut glyph: Glyph, visited: &mut Vec<bool>, pixel: Pixel) -> Glyph {
+    let surrounding: [(i32, i32); 8] = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (-1, 1),
+        (-1, 0),
+    ];
+    // List of pixels we need to address
+    let mut queue: Vec<Pixel> = vec![];
+    // The current pixel we are looking around
+    let mut selected_pixel = pixel;
+    loop {
+        // Set the visited point to true, and add the pixel to the glyph
+        visited[get_index(selected_pixel.point, image.width)] = true;
+        glyph.pixels.push(selected_pixel);
+        
+        // Gather the surrounding pixels
+        for i in 0..8 {
+            let (off_x, off_y) = surrounding[i];
+            let new_x = selected_pixel.point.x as i32 + off_x;
+            let new_y = selected_pixel.point.y as i32 + off_y;
+            // Guard statements about going out of bounds
+            if new_x < 0 || new_x >= image.width as i32 {
+                continue;
+            }
+            if new_y < 0 || new_y >= image.height as i32 {
+                continue;
+            }
+            let new_point = Point{x: new_x as usize, y: new_y as usize};
+
+            if visited[get_index(new_point, image.width)] == true {
+                continue;
+            }
+            let new_pixel = image.pixels[get_index(new_point, image.width)];
+            if new_pixel.color != pixel.color {
+                continue;
+            }
+            queue.push(new_pixel);
+        }
+        let opt_pixel = queue.pop();
+        if opt_pixel.is_none() {
+            return glyph;
+        }
+        else {
+            selected_pixel = opt_pixel.unwrap();
+        }
     }
-    if image.pixels[index].color != color {
-        return glyph;
-    }
-    glyph.pixels.push(image.pixels[index]);
-    visited[index] = true;
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x + 1, y: pos.y});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x + 1, y: pos.y + 1});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x + 1, y: pos.y - 1});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x, y: pos.y + 1});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x, y: pos.y - 1});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x - 1, y: pos.y + 1});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x - 1, y: pos.y});
-    glyph = flood_fill(image, glyph,&mut visited, color, Point{x: pos.x - 1, y: pos.y - 1});
-    return glyph;
 }
 
 /// Opens image and given the file path is correct, returns the image
@@ -115,7 +152,7 @@ fn get_index(point: Point, width: usize) -> usize {
 }
 
 fn main() {
-    let path = "blank.png";
+    let path = "blue.png";
     let main_img = open_image(path).unwrap().to_rgb8();
 
     let width = main_img.width();
@@ -130,12 +167,25 @@ fn main() {
 
     for c in 0..(height * width) {
         let c3 = (c * 3) as usize;
-        let point = get_pos(c3, width as usize);
+        let point = get_pos(c as usize, width as usize);
         let color = Color { r: data[c3], g: data[c3 + 1], b: data[c3 + 2]}; 
         image.pixels.push(Pixel{color, point});
     }
 
     let mut used_colors: Vec<Color> = vec![Color{r: 255, b: 0, g: 0}, Color{r: 0, b: 255, g: 0}, Color{r: 0, b: 0, g: 255}];
 
-    print!("{:?}", gather_glyphs(image, used_colors));
+    println!("{:?}", gather_glyphs(image, used_colors));
+}
+#[cfg(test)]
+mod test {
+    use crate::{Point, get_index, get_pos};
+
+    #[test]
+    fn test() {
+        let point = Point{x: 10, y: 10};
+        let width = 20;
+        println!("{:?}", get_index(point, width));
+        println!("{:?}", get_pos(get_index(point, width), width));
+        assert!(get_pos(get_index(point, width), width) == point);
+    }
 }
